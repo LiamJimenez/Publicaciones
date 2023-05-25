8f4db986b9a7607b61da3c2ebcf53934c3824897
LiamJimenez <liammarcos@gmail.com>
2023-05-25 16:02:52 -0400

    Se agrega el proyecto

---
+++ Publicaciones.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Publicaciones.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
