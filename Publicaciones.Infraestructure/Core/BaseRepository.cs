c248ce77097ce37c44ebd2baa604fad2991b50b8
LiamJimenez <liammarcos@gmail.com>
2023-06-01 16:14:31 -0400

    Actualiazion

--- Publicaciones.Infraestructure/Core/BaseRepository.cs
+++ Publicaciones.Infraestructure/Core/BaseRepository.cs
@@ -1,7 +1,9 @@
-﻿using Publicaciones.Infraestructure.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using Publicaciones.Infraestructure.Context;
 using Publicaciones.Infraestructure.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -10,14 +12,16 @@ namespace Publicaciones.Infraestructure.Core
     public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
         private readonly PublicacionesContext publicaciones;
+        private readonly DbSet<TEntity> entities;
 
         public BaseRepository(PublicacionesContext publicaciones) 
         {
             this.publicaciones = publicaciones;
+            this.entities = this.publicaciones.Set<TEntity>();
         }
         public bool Exists(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            return this.entities.Any(filter);
         }
 
         public IEnumerable<TEntity> GetEntities()
