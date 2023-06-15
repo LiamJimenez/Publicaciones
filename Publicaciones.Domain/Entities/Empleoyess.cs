e137fa491d958f872ca7d2244299a941c0449883
Osia Miguel Clara Reynoso <osiamiguelclarareynoso@gmail.com>
2023-06-15 09:31:54 -0400

    actualizar

--- Publicaciones.Domain/Entities/Empleoyess.cs
+++ Publicaciones.Domain/Entities/Empleoyess.cs
@@ -1,6 +1,17 @@
-﻿namespace Publicaciones.Domain.Entities
+﻿using Publicaciones.Domain.Core;
+using System;
+
+namespace Publicaciones.Domain.Entities
 {
-    internal class Empleoyess
+    public class Empleoyess: BaseEntity
     {
+        public char emp_id {get; set;}
+        public string fname {get; set;}
+        public char? minit {get; set;}
+        public string lname {get; set;}
+        public short job_id {get; set;}
+        public byte? job_lvl {get; set;}
+        public char pub_id {get; set;}
+        public DateTime hire_date {get; set;}
     }
 }
