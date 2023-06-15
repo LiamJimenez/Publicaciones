df9fcdcc7565df23dc548d1df057bfc0720416a7
Osia Miguel Clara Reynoso <osiamiguelclarareynoso@gmail.com>
2023-06-15 09:49:47 -0400

    actualizar

--- Publicaciones.Domain/Entities/Jobs.cs
+++ Publicaciones.Domain/Entities/Jobs.cs
@@ -1,6 +1,10 @@
-﻿namespace Publicaciones.Domain.Entities
+﻿using Publicaciones.Domain.Core;
+namespace Publicaciones.Domain.Entities
 {
-    internal class Jobs
+    public class Jobs : BaseEntity
     {
+        public short job_id { get; set; }
+        public string job_disc { get; set; }
+        public byte min_lvl { get; set; }
     }
 }
